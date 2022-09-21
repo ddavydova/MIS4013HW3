afa6e2d7393d676ff4ec0d1c0ba7483fb15ed576
Dasha Davydova <dasha.davydova@ou.edu>
2022-09-21 13:36:21 -0500

    Create links.php

---
+++ links.php
@@ -0,0 +1,16 @@
+<div class="container display-4">
+	<ul class="nav nav-pills nav-fill">
+          <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Main Page") {?>active<?php }?>" href="index.php">Home</a>
+	  </li>
+	  <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Table 2") {?>active<?php }?>" href="form.php">Form</a>
+	  </li>
+    <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Table 3") {?>active<?php }?>" href="form.php">Form</a>
+	  </li>
+    <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Table 4") {?>active<?php }?>" href="form.php">Form</a>
+	  </li>
+	</ul>
+</div>
