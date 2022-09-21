1fa7d3ad39504a1e2bf3ab0240a23dbe542d5f97
Dasha Davydova <dasha.davydova@ou.edu>
2022-09-21 13:34:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
+<!DOCTYPE html>
+<html>
+<head> <?php include("header.php");?></head>
+<body>
+
+<?php include("links.php");?>
+<br>
+<br>
 <table class="table table-striped">
   <thead>
     <tr>
@@ -50,61 +49,8 @@ $conn->close();
 ?>
   </tbody>
     </table>
-    
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th>Customer ID</th>
-      <th>Order ID</th>
-      <th>Product ID</th>
-      <th>Quantity</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-$servername = "localhost";
-$username = "davyddov_davyddova";
-$password = "dasha12345!";
-$dbname = "davyddov_HW3";
+  
+<?php include("footer.php");?>
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT id, orderid, pid, quantity from Order";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-?>
-  <tr>
-    <td><?=$row["id"]?></td>
-    <td><?=$row["orderid"]?></td>
-    <td><?=$row["pid"]?></td>
-    <td><?=$row["quantity"]?></td>
-  </tr>
-<?php
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
-?>
-  </tbody>
-    </table>
-    
-    <h1>Hello, world!</h1>
-    <form action="handlepost.php" method="get">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-    
-    <a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
+</body>
 </html>
