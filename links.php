10861956e637783b4d73cc1404652098cda20eda
Dasha Davydova <dasha.davydova@ou.edu>
2022-09-21 13:37:54 -0500

    Update links.php

--- links.php
+++ links.php
@@ -4,13 +4,13 @@
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Main Page") {?>active<?php }?>" href="index.php">Home</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Table 2") {?>active<?php }?>" href="form.php">Form</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Table 2") {?>active<?php }?>" href="form.php">Table 2</a>
 	  </li>
     <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Table 3") {?>active<?php }?>" href="form.php">Form</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Table 3") {?>active<?php }?>" href="form.php">Table 3</a>
 	  </li>
     <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Table 4") {?>active<?php }?>" href="form.php">Form</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Table 4") {?>active<?php }?>" href="form.php">Table 4</a>
 	  </li>
 	</ul>
 </div>
