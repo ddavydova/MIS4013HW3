0b3a64cfa5bb314d8f572564869a614715923f6d
Dasha Davydova <dasha.davydova@ou.edu>
2022-09-21 14:08:28 -0500

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head> <?php include("header.php");?></head>
@@ -7,8 +6,52 @@
 <?php include("links.php");?>
 <br>
 <br>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "davyddov_davyddova";
+$password = "dasha12345!";
+$dbname = "davyddov_HW3";
 
-<?php include("footer.php");?>
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
-</body>
+$sql = "select o.quantity, o.product_id, order_id, p.pname, c.lname from order o join customer c on o.customer_id = c.customer_id join product p on p.product_id = o.product_id where c.customer_id=" . $iid;
+//echo $sql;
+    $result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["order_od"]?></td>
+    <td><?=$row["product_id"]?></td>
+    <td><?=$row["pname"]?></td>
+    <td><?=$row["quantity"]?></td>
+    <td><?=$row["lname"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
 </html>
