ebbd0c0e7b6c34981e33bd1b0dcf2a11e58c15df
Dasha Davydova <dasha.davydova@ou.edu>
2022-09-21 14:20:01 -0500

    Update page2.php

--- page2.php
+++ page2.php
@@ -10,8 +10,6 @@
   <thead>
     <tr>
       <th>Order ID</th>
-      <th>Product ID</th>
-      <th>Product Name</th>
       <th>Quantity</th>
       <th>Customer's Last Name</th>
     </tr>
@@ -30,7 +28,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
  $iid = $_GET['id'];
-$sql = "select o.quantity, o.product_id, order_id, p.pname, c.lname from order o join customer c on o.customer_id = c.customer_id join product p on p.product_id = o.product_id where c.customer_id=" . $iid;
+$sql = "select o.quantity, order_id, c.lname from order o join customer c on o.customer_id = c.customer_id where c.customer_id=" . $iid;
 //echo $sql;
     $result = $conn->query($sql);
 
@@ -40,8 +38,6 @@ if ($result->num_rows > 0) {
 ?>
   <tr>
     <td><?=$row["order_id"]?></td>
-    <td><?=$row["product_id"]?></td>
-    <td><?=$row["pname"]?></td>
     <td><?=$row["quantity"]?></td>
     <td><?=$row["lname"]?></td>
   </tr>
