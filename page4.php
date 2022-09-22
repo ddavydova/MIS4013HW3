48e4e0d2ab94e1bab7ef9b0c897ea0f855bbe818
Dasha Davydova <dasha.davydova@ou.edu>
2022-09-21 19:51:24 -0500

    Update page4.php

--- page4.php
+++ page4.php
@@ -12,19 +12,17 @@
   <thead>
     <tr>
       <th>Customer ID</th>
-      <th>Prefix</th>
-      <th>Number</th>
-      <th>Description</th>
-      <th></th>
+      <th>Customer's First Name</th>
+      <th>Customer's Last Name</th>
     </tr>
   </thead>
   <tbody>
     <?php
 $servername = "localhost";
-$username = "projecto_homework3";
-$password = "0w_zeP}]OVy0";
-$dbname = "projecto_homework3";
-
+$username = "davyddov_davyddova";
+$password = "dasha12345!";
+$dbname = "davyddov_HW3";
+   
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -32,7 +30,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * from course";
+$sql = "SELECT * from Customer";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -40,13 +38,12 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
 ?>
   <tr>
-    <td><?=$row["course_id"]?></td>
-    <td><?=$row["prefix"]?></td>
-    <td><?=$row["number"]?></td>
-    <td><?=$row["description"]?></td>
+    <td><?=$row["customer_id"]?></td>
+    <td><?=$row["fname"]?></td>
+    <td><?=$row["lname"]?></td>
     <td>
-      <form method="post" action="course-section.php">
-        <input type="hidden" name="id" value="<?=$row["course_id"]?>" />
+      <form method="post" action="page3.php">
+        <input type="hidden" name="id" value="<?=$row["customer_id"]?>" />
         <input type="submit" value="Sections" />
       </form>
     </td>
