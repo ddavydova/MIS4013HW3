ee9af663f370df3d50e2970a3d9e43da59fc6245
Dasha Davydova <dasha.davydova@ou.edu>
2022-09-21 19:48:20 -0500

    Update page4.php

--- page4.php
+++ page4.php
@@ -7,7 +7,59 @@
 <?php include("links.php");?>
 <br>
 <br>
+ <h1>Courses</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Customer ID</th>
+      <th>Prefix</th>
+      <th>Number</th>
+      <th>Description</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "projecto_homework3";
+$password = "0w_zeP}]OVy0";
+$dbname = "projecto_homework3";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * from course";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["course_id"]?></td>
+    <td><?=$row["prefix"]?></td>
+    <td><?=$row["number"]?></td>
+    <td><?=$row["description"]?></td>
+    <td>
+      <form method="post" action="course-section.php">
+        <input type="hidden" name="id" value="<?=$row["course_id"]?>" />
+        <input type="submit" value="Sections" />
+      </form>
+    </td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
 <?php include("footer.php");?>
 
 </body>
