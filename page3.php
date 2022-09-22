5808af59fc37d69714e0bcf8717323ca90a0b997
Dasha Davydova <dasha.davydova@ou.edu>
2022-09-21 19:38:57 -0500

    Update page3.php

--- page3.php
+++ page3.php
@@ -34,7 +34,7 @@ if ($result->num_rows > 0) {
       <h5 class="card-title"><?=$row["fname"]?></h5>
       <p class="card-text"><ul>
 <?php
-    $section_sql = "select o.quantity from Customer c join Orders o on c.customer_id = o.customer_id join Product p on p.product_id = o.product_id where i.customer_id=" . $row["customer_id"];
+    $section_sql = "select o.quantity from Customer c join Orders o on c.customer_id = o.customer_id join Product p on p.product_id = o.product_id where c.customer_id=" . $row["customer_id"];
     $section_result = $conn->query($section_sql);
     
     while($section_row = $section_result->fetch_assoc()) {
